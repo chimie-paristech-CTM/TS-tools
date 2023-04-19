d63a084bc76d70fd2dd38c77c867066887fd672b
tstuyver <thijsstuyver@hotmail.com>
2023-04-19 18:11:28 +0200

    Update

--- src/reaction_profile_generator/generator.py
+++ src/reaction_profile_generator/generator.py
@@ -13,6 +13,7 @@ from autode.geom import calc_heavy_atom_rmsd
 from autode.constraints import Constraints
 from scipy.spatial import distance_matrix
 import re
+import copy
 
 ps = Chem.SmilesParserParams()
 ps.removeHs = False
@@ -52,44 +53,21 @@ def work_in(dir_ext: str) -> Callable:
 
 @work_in(workdir)
 def jointly_optimize_reactants_and_products(reactant_smiles, product_smiles):
-    # get full smiles
-    # determine active bonds
-    # set bond lengths
-    # Randomize-and-relax conformer generation
-    # use same constraints to optimize with xTB
-    # lift restrictions on the broken bonds -> reactants
-    # lift restrictions on the formed bonds -> products
-    #reactant_smiles = prepare_smiles(reactant_smiles)
     full_reactant_mol = Chem.MolFromSmiles(reactant_smiles, ps)
     full_product_mol = Chem.MolFromSmiles(product_smiles, ps)
 
     formed_bonds, broken_bonds = get_active_bonds(full_reactant_mol, full_product_mol) # right now only single bonds are considered
 
     full_reactant_dict = {atom.GetAtomMapNum(): atom.GetIdx() for atom in full_reactant_mol.GetAtoms()}
+    #full_product_dict = {atom.GetAtomMapNum(): atom.GetIdx() for atom in full_product_mol.GetAtoms()}
     full_reactant_dict_reverse = {atom.GetIdx(): atom.GetAtomMapNum() for atom in full_reactant_mol.GetAtoms()}
+    full_product_dict_reverse = {atom.GetIdx(): atom.GetAtomMapNum() for atom in full_product_mol.GetAtoms()}
 
-    formation_constraints, breaking_constraints = {}, {}
-    print(reactant_smiles, product_smiles)
-    print(formed_bonds, broken_bonds)
-    for bond in formed_bonds:
-       i,j = map(int, bond.split('-'))
-       mol_begin = full_reactant_mol.GetAtomWithIdx(full_reactant_dict[i])
-       mol_end = full_reactant_mol.GetAtomWithIdx(full_reactant_dict[j])
-       formation_constraints[(mol_begin.GetIdx(), mol_end.GetIdx())] = \
-        (covalent_radii_pm[mol_begin.GetAtomicNum()] + covalent_radii_pm[mol_end.GetAtomicNum()]) * 1.5 / 100
-
-    for bond in broken_bonds:
-        i,j = map(int, bond.split('-'))
-        atom_begin = full_reactant_mol.GetAtomWithIdx(full_reactant_dict[i])
-        atom_end = full_reactant_mol.GetAtomWithIdx(full_reactant_dict[j])
-        breaking_constraints[(atom_begin.GetIdx(), atom_end.GetIdx())] = \
-                                (covalent_radii_pm[atom_begin.GetAtomicNum()] + covalent_radii_pm[atom_end.GetAtomicNum()]) * 1.5 / 100
+    formation_constraints, breaking_constraints = get_constraints(formed_bonds, broken_bonds, full_reactant_mol, full_reactant_dict, covalent_radii_pm)
 
     get_conformer(full_reactant_mol)
-
     write_xyz_file(full_reactant_mol, 'input.xyz')
-
-    ade_mol = ade.Molecule('input.xyz', charge=0) #TODO: stereochemistry???
+    ade_mol = ade.Molecule('input.xyz', charge=0) #TODO: stereochemistry??? -> this should be postprocessed by adding constraints like in autode
 
     constraints = formation_constraints.copy()
     constraints.update(breaking_constraints)
@@ -109,61 +87,90 @@ def jointly_optimize_reactants_and_products(reactant_smiles, product_smiles):
 
         conformer.optimise(method=xtb)
         conformers.append(conformer)
+    # THESE CONFORMERS ARE ACTUALLY ALREADY GOOD ENOUGH IN QUALITY!!!!! -> simply figure out wtf is wrong with xtb-gaussian, and/or just do freq calculation to decide constraint magnitude
 
     conformers = prune_conformers(conformers, rmsd_tol = 0.1) # angström
+    for conformer in conformers:
+        print(conformer.energy)
 
-    # get force constant for xtb
-    #r_conf = conformers[0].copy()
-    #r_conf.name = f'r_conf'
-    #r_conf.constraints = Constraints()
-    #for key, val in formation_constraints.items():
-    #    r_conf.constraints.update({key: val * 3})
-    #r_conf.optimise(method=ade.methods.XTB())
-    breaking_constraints_final = determine_forces(reactant_smiles, breaking_constraints, full_reactant_dict_reverse)
+    breaking_constraints_final = determine_forces(reactant_smiles, broken_bonds, full_reactant_dict, full_reactant_dict_reverse)
+    formation_constraints_final = determine_forces(product_smiles, formed_bonds, full_reactant_dict, full_product_dict_reverse)
 
     for i, conformer in enumerate(conformers):
+        new_conf = conformer.copy()
+        new_conf.constraints = Constraints()
+        new_conf.name = f'test_{i}'
+        xtb.force_constant = 2
+        for key, val in breaking_constraints_final.items(): # TODO: maybe this needs to happen earlier???
+            new_conf.constraints.update({key: val[0] * 1.40})
+        for key, val in formation_constraints_final.items():
+            new_conf.constraints.update({key: val[0] * 1.40})
+        new_conf.optimise(method = xtb)
+
+
         energies = []
-        for distance in range(5, 90):
-            new_conf = conformer.copy()
-            new_conf.constraints = Constraints()
-            for key, val in breaking_constraints_final.items(): # maybe this should switch positions in nested loop???
-                new_conf.name = f'final_{i}_{distance/100}'
-                new_conf.constraints.update({key: val[0] + distance/100})
-            xtb.force_constant = list(breaking_constraints_final.values())[0][1] # TODO: fix this!!!
-            new_conf.optimise(method = xtb)
-            energies.append(new_conf.energy)
+        final_key, final_val = list(breaking_constraints_final.items())[0]
+        for distance in range(10, 40):
+            xtb.force_constant = final_val[1]
+            final_conf = new_conf.copy()
+            final_conf.constraints = Constraints()
+            final_conf.name = f'final_{i}_{distance/100}'
+            final_conf.constraints.update({final_key: final_val[0] + distance/100})
+            xtb.force_constant = 2 * final_val[1] # TODO: fix this!!!
+            final_conf.optimise(method = xtb)
+            energies.append(final_conf.energy)
             
-        print(energies, range(5, 90)[energies.index(max(energies))])
-        raise KeyError
+        print(energies, range(10, 30)[energies.index(max(energies))])
+        #raise KeyError
                 #dist_matrix = distance_matrix(conformer._coordinates, conformer._coordinates) # you probably want to ensure that bond distances are longer than product distances
                 #print(dist_matrix)
                 #print('')
         
 
+def get_constraints(formed_bonds, broken_bonds, full_reactant_mol, full_reactant_dict, radii_dict):
+    formation_constraints, breaking_constraints = {}, {}
 
+    for bond in formed_bonds:
+       i,j = map(int, bond.split('-'))
+       mol_begin = full_reactant_mol.GetAtomWithIdx(full_reactant_dict[i])
+       mol_end = full_reactant_mol.GetAtomWithIdx(full_reactant_dict[j])
+       formation_constraints[(mol_begin.GetIdx(), mol_end.GetIdx())] = \
+        (radii_dict[mol_begin.GetAtomicNum()] + radii_dict[mol_end.GetAtomicNum()]) * 1.5 / 100
 
+    for bond in broken_bonds:
+        i,j = map(int, bond.split('-'))
+        atom_begin = full_reactant_mol.GetAtomWithIdx(full_reactant_dict[i])
+        atom_end = full_reactant_mol.GetAtomWithIdx(full_reactant_dict[j])
+        breaking_constraints[(atom_begin.GetIdx(), atom_end.GetIdx())] = \
+                                (radii_dict[atom_begin.GetAtomicNum()] + radii_dict[atom_end.GetAtomicNum()]) * 1.5 / 100
+
+    return formation_constraints, breaking_constraints
 
 
 # you don't want the other molecules to be there as well
-def determine_forces(reactant_smiles, breaking_constraints, full_reactant_dict_reverse):
-    reactant_mols = [Chem.MolFromSmiles(smi, ps) for smi in reactant_smiles.split('.')]
+def determine_forces(smiles, active_bonds, full_reactant_dict, full_mol_dict_reverse):
+    #print(smiles, constraints, full_mol_dict_reverse)
+    mols = [Chem.MolFromSmiles(smi, ps) for smi in smiles.split('.')]
     owning_mol_dict = {}
-    for idx, mol in enumerate(reactant_mols):
+    for idx, mol in enumerate(mols):
         for atom in mol.GetAtoms():
             owning_mol_dict[atom.GetAtomMapNum()] = idx
 
-    breaking_constraints_final = {}
+    constraints = {}
 
     xtb.force_constant = 5
 
-    for i,j in breaking_constraints:
-        if owning_mol_dict[full_reactant_dict_reverse[i]] == owning_mol_dict[full_reactant_dict_reverse[j]]:
-            mol = reactant_mols[owning_mol_dict[full_reactant_dict_reverse[i]]]
+    for bond in active_bonds:
+        i,j = map(int, bond.split('-'))
+        print(i,j, owning_mol_dict[i], owning_mol_dict[j], smiles)
+        if owning_mol_dict[i] == owning_mol_dict[j]:
+            mol = copy.deepcopy(mols[owning_mol_dict[i]])
         else:
             print("WTF")
             raise KeyError
     
         mol_dict = {atom.GetAtomMapNum(): atom.GetIdx() for atom in mol.GetAtoms()}
+        print(mol_dict)
         [atom.SetAtomMapNum(0) for atom in mol.GetAtoms()]
 
         # detour needed to avoid reordering of the atoms by autodE
@@ -175,17 +182,16 @@ def determine_forces(reactant_smiles, breaking_constraints, full_reactant_dict_r
 
         ade_rmol.conformers[0].optimise(method=xtb)
         dist_matrix = distance_matrix(ade_rmol.conformers[0]._coordinates, ade_rmol.conformers[0]._coordinates)
-        current_bond_length = dist_matrix[mol_dict[full_reactant_dict_reverse[i]], mol_dict[full_reactant_dict_reverse[j]]]
+        current_bond_length = dist_matrix[mol_dict[i], mol_dict[j]]
         x0 = np.linspace(current_bond_length - 0.05, current_bond_length + 0.05, 5)
-        energies = stretch(ade_rmol.conformers[0], x0, (mol_dict[full_reactant_dict_reverse[i]], mol_dict[full_reactant_dict_reverse[j]])) # make sure this is ok
+        energies = stretch(ade_rmol.conformers[0], x0, (mol_dict[i], mol_dict[j])) # make sure this is ok
         
         p = np.polyfit(np.array(x0), np.array(energies), 2)
 
-        force_constant = float(p[0] * 2 * bohr_ang)
-        print(force_constant)  
-        breaking_constraints_final[(i,j)] = [current_bond_length, force_constant]
+        force_constant = float(p[0] * 2 * bohr_ang) 
+        constraints[(full_reactant_dict[i],full_reactant_dict[j])] = [current_bond_length, force_constant]
     
-    return breaking_constraints_final
+    return constraints
 
 
 def stretch(conformer, x0, bond):
@@ -193,7 +199,7 @@ def stretch(conformer, x0, bond):
 
     for length in x0:
         conformer.constraints.update({bond: length})
-        conformer.name = f'x_{length}'
+        conformer.name = f'stretch_{length}'
         conformer.optimise(method=ade.methods.XTB())
         energies.append(conformer.energy)
     
